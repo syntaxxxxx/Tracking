1eb517fe58fedab6ce00c0f87b10109f40476e38
Fiqri Hafzain <fiqrihafzainislami@gmail.com>
2019-02-12 03:29:38 +0700

    first commit

---
+++ app/src/main/java/com/syntax/trackingrealtime/activities/Register.java
@@ -0,0 +1,15 @@
+package com.syntax.trackingrealtime.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.syntax.trackingrealtime.R;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
